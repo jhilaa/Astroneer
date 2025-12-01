8bc5e550b85e34471c87cfc307f4a3472f239d1d
Julien Hilaire <julien.hilaire@imelmi.net>
2025-12-01 20:16:23 +0100

    dissociation ressources atmos. et par planètes

--- python/scraping/resources/process_atmospheric_resources_data.py
+++ python/scraping/resources/process_atmospheric_resources_data.py
@@ -13,6 +13,7 @@ def atmospheric_resources_data_to_json(soup: BeautifulSoup):
     ENDPOINT_ATMOSPHERIC_RESOURCE = utils.get_env("ENDPOINT_ATMOSPHERIC_RESOURCE")
     
     atmospheric_resources = []
+    atmospheric_resources_concentration = []
     start = soup.select_one("#Ressources_atmosphériques")
     if start:
         table = start.find_next("table")
@@ -25,20 +26,25 @@ def atmospheric_resources_data_to_json(soup: BeautifulSoup):
                 resource_name = cells[0].get_text(strip=True)
                 icon = cells[0].find("img")
                 icon_data_src = icon.get("data-src") if icon else None
-
-                planet_concentration_data = []
-                for i, cell in enumerate(cells[1:]):
-                    concentration = cell.get_text(strip=True)
-                    planet_concentration_data.append({"planete": planet_names[i], "taux": concentration})
-
+                
+                # Données référentielles
                 atmospheric_resources.append({
                     "name": resource_name,
-                    "icon_data_src": icon_data_src,
-                    "concentration": planet_concentration_data})
-                
+                    "icon_data_src": icon_data_src})
+                # Données par planète
+                for i, cell in enumerate(cells[1:]):
+                    concentration = cell.get_text(strip=True)
+                    atmospheric_resources_concentration.append ({
+                        "resource_name" : resource_name,
+                        "planete": planet_names[i],
+                        "taux": concentration})
+ 
             script_dir = os.path.dirname(os.path.abspath(__file__)) 
-            utils.create_json_file (dir_name=script_dir, dataset_name="atmospheric_resources", json_data=atmospheric_resources)   
-            
+            # Données référentielles
+            utils.create_json_file (dir_name=script_dir, dataset_name="atmospheric_resources", json_data=atmospheric_resources)         
+            # Données par planète
+            utils.create_json_file (dir_name=script_dir, dataset_name="atmospheric_resources_concentration", json_data=atmospheric_resources_concentration)   
+              
             # On envoie le JSON au service REST
             headers = {"Content-Type": "application/json"}
             #response = requests.post(ENDPOINT_ATMOSPHERIC_RESOURCE, json=atmospheric_resources, headers=headers)
